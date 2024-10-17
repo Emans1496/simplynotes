1688ca27e3017644e3fc4e5fe7a94d5bb62ee33c
Emanuele <emanuelesquillante@libero.it>
2024-10-17 20:49:36 +0200

    default

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 // index.php
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://simplynotes-static.onrender.com");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
@@ -8,12 +8,12 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 require_once '../config/db.php';
 require_once '../routes/Router.php';
 
-if (\$_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     // Rispondi con 200 OK per le richieste preflight
     header("HTTP/1.1 200 OK");
     exit();
 }
 
-\$router = new Router();
-\$router->direct(\$_SERVER['REQUEST_URI'], \$_SERVER['REQUEST_METHOD']);
+$router = new Router();
+$router->direct($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 ?>
