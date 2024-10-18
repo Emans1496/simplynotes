075e61ee779110e25ab485f8c481c3372ee089c1
Emanuele <emanuelesquillante@libero.it>
2024-10-18 15:13:43 +0200

    origin

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://simplynotes-production.up.railway.app/");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
