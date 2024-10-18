15c94b1b21ae59ef5928845c9f9fccd7e87ab9e7
Emanuele <emanuelesquillante@libero.it>
2024-10-18 21:51:02 +0200

    200

--- api/Router.php
+++ api/Router.php
@@ -1,11 +1,10 @@
-// Router.php
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 
-require_once '../controllers/NoteController.php';
-require_once '../controllers/UserController.php';
+require_once './controllers/NoteController.php';
+require_once './controllers/UserController.php';
 
 class Router {
     public function direct($uri, $method) {
--- api/index.php
+++ api/index.php
@@ -1,11 +1,11 @@
-// index.php
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 
 require_once '../config/db.php';
-require_once '../routes/Router.php';
+require_once './Router.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("HTTP/1.1 200 OK");
