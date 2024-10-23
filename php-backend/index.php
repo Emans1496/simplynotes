e550821ea61c276e3b9a4ddfef75539c81dcd4d3
Emanuele <emanuelesquillante@libero.it>
2024-10-23 17:34:15 +0200

    CORS

--- php-backend/index.php
+++ php-backend/index.php
@@ -1,5 +1,7 @@
 <?php
-// Carica i file necessari
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 require_once __DIR__ . '/vendor/autoload.php';
 require_once 'routes/register.php';
 require_once 'routes/login.php';
